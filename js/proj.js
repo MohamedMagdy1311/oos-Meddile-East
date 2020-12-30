504b3886716fdf6b9046cc0d5c5b8076660009bc
FatmaRida <53396911+FatmaRida@users.noreply.github.com>
2020-12-30 22:19:01 +0200

    upload site
    upload site

---
+++ js/proj.js
@@ -0,0 +1,33 @@
+
+ new WOW().init();
+
+ // start red message
+   function showpanel(){
+                
+                document.getElementById('panel').style.display="block";
+                
+            }
+            
+            function hidepanel(){
+                   document.getElementById('panel').style.display="none";
+                
+            }
+
+   // end red message
+
+
+  // start social media
+
+      function showSocialMedia(){
+                
+                document.getElementById('social-media').style.display="block";
+                
+            }
+            
+            function hideSocialMedia(){
+                   document.getElementById('social-media').style.display="none";
+                
+            }
+            
+
+
